165ad565c39992ece7130537453c15566bb6fb09
Micaela Lechmann <micaela.lechmann@ifood.com.br>
2022-01-22 19:03:45 -0300

    chore: configure sass and fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.scss';
+import './fonts/Montserrat/Montserrat-SemiBold.ttf';
+import './fonts/Roboto/Roboto-Regular.ttf';
 
 ReactDOM.render(<App />, document.getElementById('root'));
