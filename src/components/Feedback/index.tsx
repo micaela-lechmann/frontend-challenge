2dca6b1b8367c0dca2d64993cbf1cbf73ec24c9e
Micaela Lechmann <micaela.lechmann@ifood.com.br>
2022-01-25 01:06:44 -0300

    feat: create feedback component

---
+++ src/components/Feedback/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ReactComponent as SuccessIcon } from '../../static/icons/success.svg';
+import { ReactComponent as ErrorIcon } from '../../static/icons/error.svg';
+import Button from '../Button';
+
+import './styles.scss';
+
+type Props = {
+  type: 'success' | 'error';
+  description: string;
+  onClick: () => void;
+};
+
+const Feedback = ({ type, description, onClick }: Props) => {
+  return (
+    <div className='feedback'>
+      <h1 className='feedback__title'>
+        {type === 'success' ? 'Success' : 'Error'}
+      </h1>
+      {type === 'success' ? <SuccessIcon /> : <ErrorIcon />}
+      <p className='feedback__description'>{description}</p>
+      <Button type='primary' onClick={onClick}>
+        Restart
+      </Button>
+    </div>
+  );
+};
+
+export default Feedback;
