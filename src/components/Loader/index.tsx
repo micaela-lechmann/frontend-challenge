6d22581926e0feff8882e4e5a8d1c49c9033bed0
Micaela Lechmann <micaela.lechmann@ifood.com.br>
2022-01-27 10:56:27 -0300

    chore: add aria to loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,10 +1,15 @@
 import clsx from 'clsx';
 import React from 'react';
+
 import './styles.scss';
 
 const Loader = ({ show }: { show: boolean }) => {
   return (
-    <div className={clsx('loader', { 'loader--hidden': !show })}>
+    <div
+      data-testid='loader'
+      aria-hidden={!show}
+      className={clsx('loader', { 'loader--hidden': !show })}
+    >
       <div className='loader__dot loader__dot--first'></div>
       <div className='loader__dot loader__dot--second'></div>
       <div className='loader__dot loader__dot--third'></div>
